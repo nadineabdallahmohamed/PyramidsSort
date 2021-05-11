9e707b5c39bd06ba5f56a531d343a8e2c3255272
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-11 22:32:59 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,47 @@
+package Pyramidwithsort;
+
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<Pyramid> pyramids =new LinkedList<Pyramid>();
+		PyramidDAO pr = new PyramidDAO();
+		try {
+			 pyramids = pr.readcsv("C:\\java\\pyramids_sort.csv");
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+		
+		  Collections.sort(pyramids, new Pyramidsort().reversed());
+		  
+		  int i =1;
+	        for(Pyramid p: pyramids) {
+	           System.out.println("#"+(i++)+","+p.getPharaoh()+"," + p.getModern_name()+","+p.getSite()+","+p.getHeight());}
+	        Map < String, Integer > NumOfSites= new LinkedHashMap<>();
+	         for(Pyramid p: pyramids)
+	         {
+	             String site = p.getSite();
+	             Integer counter = NumOfSites.get(site);
+	             if (counter == null )
+	             { 
+	                 counter =1;
+	             }
+	             else {
+	                 counter++;
+	             } 
+	             NumOfSites.put(site, counter);
+	         } 
+	                
+	           System.out.println("\n"+NumOfSites);    
+
+	}
+
+}
