f162d8a9cb16c3b5bc7fcdd0b5627329e5696d04
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-11 23:38:53 +0200

    Update and rename Sorted_pyramids.java to Sorted_Pyramids.java

--- Sorted_Pyramids.java
+++ Sorted_Pyramids.java
@@ -2,10 +2,10 @@ package SortedPyramids;
 
 import java.util.Comparator;
 
-public class Pyramidsort  implements Comparator<Pyramid>{
+public class Sorted_Pyramids  implements Comparator<Sort>{
 
 	@Override
-	public int compare(Pyramid o1, Pyramid o2) {
+	public int compare(Sort o1, Sort o2) {
 		// TODO Auto-generated method stub
 		return  Double.valueOf(o1.getHeight()).compareTo(o2.getHeight());
 	}
